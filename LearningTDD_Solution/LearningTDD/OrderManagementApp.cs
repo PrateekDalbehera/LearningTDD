9d85312b6e61b5cbca2c3e777197642c4a821b9a
H261279 <prateek.dalbehera@honeywell.com>
2020-05-20 00:10:13 +0530

    Set up Dependency Injection manualy as it is not natively available in the Console App Template

---
+++ LearningTDD_Solution/LearningTDD/OrderManagementApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LearningTDD
+{
+    public class OrderManagementApp
+    {
+        // Inject Dependencies via Constructor.
+        public OrderManagementApp()
+        {
+
+        }
+
+        /// <summary>
+        /// This function is responsible to start the application
+        /// when called from Program.cs file.
+        /// </summary>
+        public void Run()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Application started successfully after setting up the DI Container.");
+            Console.Read();
+        }
+    }
+}
--- LearningTDD_Solution/LearningTDD/Program.cs
+++ LearningTDD_Solution/LearningTDD/Program.cs
@@ -1,13 +1,59 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
 
 namespace LearningTDD
 {
     class Program
     {
+        private static string _CONFIG_FILE = "appsettings.json";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Read();
+			try
+			{
+                Console.WriteLine("Starting Order Management System...");
+
+                var services = ConfigureServices();
+
+                // Create Provider to use Dependency Injector
+                var serviceProvider = services.BuildServiceProvider();
+
+                // Start the actual execution of the application.
+                serviceProvider.GetService<OrderManagementApp>().Run();
+            }
+			catch (Exception ex)
+			{
+                Console.WriteLine($@"Something went wrong, an error occurred.
+                        Error Message: {ex.Message}
+                        StackTrace: {ex.StackTrace}");
+                Console.Read();
+			}
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            // Set up the objects we need to get to configuration settings
+            var config = LoadConfiguration();
+
+            services.AddSingleton(config);
+
+            // IMPORTANT: Register the application entry point.
+            services.AddTransient<OrderManagementApp>();
+
+            return services;
+        }
+
+        private static IConfiguration LoadConfiguration()
+        {
+            return new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile(_CONFIG_FILE, optional: true, reloadOnChange: true)
+
+                    .Build();
         }
     }
 }
